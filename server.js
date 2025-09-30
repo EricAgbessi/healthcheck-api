fd25db8eba373fc13f373a4e28602985095f835c
EricAgbessi <agbessi558@gmail.com>
2025-09-30 12:06:02 +0100

    jouter de la route /health/add et restest du pipeline

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/health', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/health/add',(req, res)=>{
+  res.send('Tu peu ajouter un nous health check')
+})
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
